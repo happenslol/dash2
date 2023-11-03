3a75c130684cf43f9b39080bd8817fd205c7f1f2
Hilmar Wiegand <me@hwgnd.de>
2023-11-04 00:02:19 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      libraries = with pkgs; [
+        webkitgtk
+        gtk3
+        gtk-layer-shell
+        cairo
+        gdk-pixbuf
+        glib
+        dbus
+        librsvg
+      ];
+
+      packages = with pkgs; [
+        curl
+        wget
+        pkg-config
+        dbus
+        glib
+        gtk3
+        gtk-layer-shell
+        libsoup
+        webkitgtk
+        librsvg
+      ];
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = packages;
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+          export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+        '';
+      };
+    });
+}
