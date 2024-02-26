8953b717635a6ece6c31ee366fd6da64d63f8417
Hilmar Wiegand <me@hwgnd.de>
2024-02-26 23:39:01 +0100

    Update proof of concept

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,47 +2,68 @@
 
 use anyhow::Result;
 use gdk::{prelude::Cast, Monitor};
-use gtk::traits::{ContainerExt, WidgetExt};
+use gtk::{
+  prelude::GtkWindowExt,
+  traits::{ContainerExt, WidgetExt},
+};
 use webkit2gtk::WebViewExt;
 
 fn main() {
-  tauri::Builder::new()
-    .setup(|app| {
-      let display = gdk::Display::default().unwrap();
-      let n_monitors = display.n_monitors();
+  let app = tauri::Builder::default()
+    .build(tauri::generate_context!())
+    .expect("error while building tauri application");
 
-      let mut overlay_windows = Vec::new();
-      for i in 0..n_monitors {
-        let monitor = display.monitor(i).unwrap();
-        let monitor_geometry = monitor.geometry();
+  let display = gdk::Display::default().unwrap();
 
-        let top = create_overlay_window(
-          app,
-          &format!("overlay-top-{}", i),
-          "edge.html",
-          &monitor,
-          gtk_layer_shell::Layer::Top,
-          (true, false, false, false),
-          (monitor_geometry.width(), 15),
-        )?;
+  let init_window = tauri::WindowBuilder::new(&app, "init", tauri::WindowUrl::App("".into()))
+    .visible(false)
+    .build()
+    .expect("failed to create init window");
 
-        let bottom = create_overlay_window(
-          app,
-          &format!("overlay-bottom-{}", i),
-          "edge.html",
-          &monitor,
-          gtk_layer_shell::Layer::Top,
-          (false, false, true, false),
-          (monitor_geometry.width(), 15),
-        )?;
+  let gtk_app = init_window
+    .gtk_window()
+    .expect("failed to get window for init app")
+    .application()
+    .expect("failed to get application from init window");
 
-        overlay_windows.push((top, bottom));
-      }
+  init_window.close().unwrap();
 
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  let main_monitor = display.monitor(0).unwrap();
+  let top_window = gtk::ApplicationWindow::new(&gtk_app);
+
+  gtk_layer_shell::init_for_window(&top_window);
+  gtk_layer_shell::set_monitor(&top_window, &main_monitor);
+  gtk_layer_shell::set_layer(&top_window, gtk_layer_shell::Layer::Top);
+
+  gtk_layer_shell::set_anchor(&top_window, gtk_layer_shell::Edge::Top, true);
+  gtk_layer_shell::set_anchor(&top_window, gtk_layer_shell::Edge::Right, true);
+  gtk_layer_shell::set_anchor(&top_window, gtk_layer_shell::Edge::Left, true);
+
+  top_window.set_resizable(false);
+  top_window.set_decorated(false);
+  let width = main_monitor.geometry().width();
+  top_window.set_size_request(width, 15);
+  top_window.set_app_paintable(true);
+
+  top_window.show_all();
+
+  create_overlay_window(
+    &app,
+    "test",
+    "test.html",
+    &main_monitor,
+    gtk_layer_shell::Layer::Top,
+    (false, false, false, false),
+    (400, 400),
+  )
+  .expect("failed to create test window");
+
+  top_window.connect_enter_notify_event(|_window, _ev| {
+    println!("entered top window");
+    gtk::Inhibit(true)
+  });
+
+  app.run(|_, _| {});
 }
 
 fn create_overlay_window(
@@ -84,12 +105,12 @@ fn create_overlay_window(
   gtk_window.set_size_request(width, height);
   gtk_window.set_app_paintable(true);
 
-  gtk_window.connect_enter_notify_event(|window, ev| {
+  gtk_window.connect_enter_notify_event(|_window, _ev| {
     println!("entered");
     gtk::Inhibit(true)
   });
 
-  gtk_window.connect_leave_notify_event(|window, ev| {
+  gtk_window.connect_leave_notify_event(|_window, _ev| {
     println!("left");
     gtk::Inhibit(true)
   });
--- src/index.tsx
+++
@@ -1,4 +0,0 @@
-/* @refresh reload */
-import { render } from "solid-js/web"
-
-render(() => <div />, document.getElementById("root") as HTMLElement)
